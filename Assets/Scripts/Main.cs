176b3534b07b43df04fd8f29b70ee3d75ae6d5a8
artyom <goastgavrish444@gmail.com>
2021-09-11 19:02:12 +0300

    Load Aseets and Scripts

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public class Main : MonoBehaviour
+    {
+        [SerializeField] private SpriteAnimatorConfig _playerConfig;
+        [SerializeField] private int _animationSpeed = 10;
+        [SerializeField] private LevelObjectView _playerView;
+        private SpriteAnimatorController _playerAnimator;
+
+
+        void Awake()
+        {
+            _playerConfig = Resources.Load<SpriteAnimatorConfig>("PlayerAnimCfg");
+            _playerAnimator = new SpriteAnimatorController(_playerConfig);
+            _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Run, true, _animationSpeed);
+        }
+
+
+        void Update()
+        {
+            _playerAnimator.Update();
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+    }
+}
